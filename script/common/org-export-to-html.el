46ac507099594db051292b6c0ff9112cb3eb53ad
Yang Liu <ayliu@amazon.com>
2018-07-05 12:39:26 +0800

    The github svg should be ?sanitized=true.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- script/common/org-export-to-html.el
+++ script/common/org-export-to-html.el
@@ -1,7 +1,6 @@
 :;exec emacs.sh -l "$0" -f main "$@"
 (require 'cl)
 (toggle-debug-on-error)
-(require 'org)
 
 (defun locate-dir-in-dir (dir subdir-prefix)
   (let* ((absdir (expand-file-name dir))
@@ -15,11 +14,38 @@
       (add-to-list 'load-path maybe-dir)
     (error "no org-mode lisp files found")))
 
+(require 'org)
 (require 'ox)
+(require 'ox-html)
+
+(defconst svg-host-url-mapping
+  '(("^https://github\\.com/\\\(.*\\\)/blob/\\\(.*\\.svg\\\)$"
+     .
+     "https://rawgithub.com/\\1/\\2?sanitize=true")))
+
+(defun* org-html--svg-image-hosting (source attributes info)
+  "Embedding svg file for github raw image"
+  (dolist (m svg-host-url-mapping)
+    (when (string-match (car m) source)
+      (return-from org-html--svg-image-hosting
+        (org-html-close-tag
+         "img"
+         (org-html--make-attribute-string
+          (org-combine-plists
+           (list :src (replace-regexp-in-string (car m) (cdr m) source)
+	             :alt (file-name-nondirectory source))
+           attributes))
+         info)
+        ))))
+(advice-add #'org-html--svg-image :before-until
+            #'org-html--svg-image-hosting)
 
 (defun main ()
   (interactive)
-  (destructuring-bind (file &optional (outputfile nil)) command-line-args-left
-    (print file outputfile)
+  (destructuring-bind (file &optional output-file) command-line-args-left
     (with-current-buffer (find-file-noselect file)
-      (org-export-to-file 'html (concat (file-name-sans-extension file) ".html") nil nil nil t))))
+      (org-export-to-file
+          'html
+          (or output-file
+              (concat (file-name-sans-extension file) ".html"))
+        nil nil nil t))))
