f4b7ac74baf555aace5dfad2f1c8e93cf94155d0
Yang Liu <ayliu@amazon.com>
2018-07-06 21:33:48 +0800

    Add lightbox2 support.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- script/common/org-export-to-html.el
+++ script/common/org-export-to-html.el
@@ -2,23 +2,20 @@
 (require 'cl)
 (toggle-debug-on-error)
 
-(defun locate-dir-in-dir (dir subdir-prefix)
+(defun add-to-load-path-maybe (dir subdir-prefix)
   (let* ((absdir (expand-file-name dir))
          (maybe-exist-name (file-name-completion subdir-prefix absdir)))
-    (if maybe-exist-name
-        (concat absdir "/" maybe-exist-name)
-      nil)))
+    (when maybe-exist-name
+      (add-to-list 'load-path (concat absdir "/" maybe-exist-name)))))
 
-(let ((maybe-dir (locate-dir-in-dir "~/.emacs.d/elpa/" "org-20")))
-  (if maybe-dir
-      (add-to-list 'load-path maybe-dir)
-    (error "no org-mode lisp files found")))
+(add-to-load-path-maybe "~/.emacs.d/elpa/" "org-20")
+(add-to-load-path-maybe "~/.emacs.d/elpa/" "htmlize-")
 
 (require 'org)
 (require 'ox)
 (require 'ox-html)
 
-(defconst svg-host-url-mapping
+(defconst image-source-matching-alist
   '(("^https://github\\.com/\\\(.*\\\)/blob/\\\(.*\\.svg\\\)$"
      .
      "https://rawgithub.com/\\1/\\2?sanitize=true")
@@ -47,41 +44,77 @@
     (when (or (not (file-exists-p target-file))
               (not (equal (sha1sum-of-file source-file) (sha1sum-of-file target-file))))
       (make-directory target-dir t)
-      (copy-file source-file target-file))
+      (copy-file source-file target-file t))
     (format "/assets/images/%s.%s" basename extension)))
 
-(defun* org-html--svg-image-hosting (source attributes info)
-  "Embedding svg file for github raw image"
-  (message "call with source=[%s]" source)
-  (dolist (m svg-host-url-mapping)
-    (when (string-match (car m) source)
-      (message "found a match[%s]" (car m))
-      (let ((source-link (if (stringp (cdr m))
-                             (replace-regexp-in-string (car m) (cdr m) source)
-                           (funcall (cdr m ) (car m) source))))
-        (return-from org-html--svg-image-hosting
+(defconst pre-defined-attributes-for-images
+  (list :class "img-fluid img-thumbnail"
+        :use-lightbox "2"))
+
+(defun lightbox-1-image-string (source attributes info)
+  (org-html-close-tag
+   "img"
+   (org-html--make-attribute-string
+    (org-combine-plists
+     (list :src source
+	       :alt (file-name-nondirectory source))
+     attributes
+     (list :data-remote source
+           :data-toggle "lightbox"
+           :data-type "image")))
+   info))
+
+(defun lightbox-2-image-string (source attributes info)
+  (format "<a href=\"%s\" data-lightbox=\"image\">%s</a>"
+          source
           (org-html-close-tag
            "img"
            (org-html--make-attribute-string
             (org-combine-plists
-             (list :src source-link
-	               :alt (file-name-nondirectory source)
-                   :class "img-thumbnail img-fluid"
-                   :data-remote source-link
-                   :data-toggle "lightbox"
-                   :data-type "image")
+             (list :src source
+                   :alt (file-name-nondirectory source))
              attributes))
-           info))))))
+           info)))
 
-(advice-add #'org-html--svg-image :before-until
-            #'org-html--svg-image-hosting)
+(defun* org-html--format-image-add-attributes (source attributes)
+  (let ((attrs
+         (org-combine-plists
+          pre-defined-attributes-for-images
+          attributes)))
+    attrs))
+
+(defun* org-html--format-image-new (source attributes info)
+  "Embedding svg file for github raw image"
+  (message "call with source=[%s]" source)
+  (let* ((attrs (org-html--format-image-add-attributes source attributes))
+         (make-string-func (if (equal (plist-get attrs :use-lightbox) "1")
+                               'lightbox-1-image-string
+                             'lightbox-2-image-string)))
+    (dolist (m image-source-matching-alist)
+      (when (string-match (car m) source)
+        (message "found a match[%s]" (car m))
+        (let ((source-link (if (stringp (cdr m))
+                               (replace-regexp-in-string (car m) (cdr m) source)
+                             (funcall (cdr m ) (car m) source))))
+          (return-from org-html--format-image-new
+            (funcall make-string-func source-link attrs info)))))
+    (funcall make-string-func source attrs info)))
+
+(advice-add #'org-html--format-image :override
+            #'org-html--format-image-new)
 
 (defun main ()
   (interactive)
   (destructuring-bind (file &optional output-file) command-line-args-left
     (with-current-buffer (find-file-noselect file)
-      (org-export-to-file
-          'html
-          (or output-file
-              (concat (file-name-sans-extension file) ".html"))
-        nil nil nil t))))
+      ;; the org-html-htmlize-output-type controls whether export code as
+      ;; css or inline-css
+      ;; css: export selectors only
+      ;; inline-css: selector and attributes
+      ;; nil: plain text.
+      (let ((org-html-htmlize-output-type 'nil))
+        (org-export-to-file
+            'html
+            (or output-file
+                (concat (file-name-sans-extension file) ".html"))
+          nil nil nil t)))))
