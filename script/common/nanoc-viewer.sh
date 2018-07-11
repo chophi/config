d7972944ed3e033fe487d011d441cf910d18b1b4
Yang Liu <ayliu@amazon.com>
2018-07-11 16:02:58 +0800

    Add nanoc-viewer.sh.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

---
+++ script/common/nanoc-viewer.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function bin_exist_p {
+    if [ "`which $1`" == "" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+if !(bin_exist_p fswatch); then
+    echo "You should install fswatch first"
+    exit 0;
+fi
+
+function do_clean_up {
+    killall nanoc 2>/dev/null
+    killall fswatch 2>/dev/null
+}
+
+public_dir=~/nanoc-site
+private_dir=~/nanoc-site-private
+
+if [ "$1" != "public" ] && [ "$1" != "private" ]; then
+    do_clean_up
+    exit 0;
+fi
+
+if [ "$1" == "public" ]; then
+    root_dir=$public_dir
+    port=3000
+else
+    root_dir=$private_dir
+    port=3001
+fi
+
+do_clean_up
+
+cd $root_dir
+fifo_name=/tmp/`basename $root_dir`.fifo
+
+mkfifo $fifo_name 2>/dev/null
+
+fswatch . --exclude='tmp/.*' --exclude='output/.*' > $fifo_name &
+nanoc view --port=$port &
+while [ -n "loop forever" ]; do
+    < $fifo_name read -t 1 input_content
+    if [ -n "$input_content" ]; then
+        echo "Changes in below files:"
+        echo "======================="
+        while [ -n "$input_content" ]; do
+            echo $input_content
+            < $fifo_name read -t 1 input_content
+        done
+        echo "======================="
+        echo "Nanoc updating ........"
+        nanoc
+    fi
+done
+
