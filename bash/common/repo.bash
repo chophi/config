4f2b65af9376f8e58abe75dd782cfcff64ef8193
Yang Liu <ayliu@amazon.com>
2018-07-12 16:19:03 +0800

    The REPO_URL should have no trailing '/'.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- bash/common/repo.bash
+++ bash/common/repo.bash
@@ -1,2 +1,2 @@
-export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo/'
+export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
 alias repo-init-android-o='repo init -u https://aosp.tuna.tsinghua.edu.cn/platform/manifest -b android-8.0.0_r13'
