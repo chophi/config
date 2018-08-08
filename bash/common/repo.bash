020954908f871a173ed46302c86862c33d8ee307
chophi <chophi@qq.com>
2018-08-08 22:31:03 +0800

    alias repo-init-android-p
    Signed-off-by: chophi <chophi@qq.com>

--- bash/common/repo.bash
+++ bash/common/repo.bash
@@ -1,2 +1,3 @@
 export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
 alias repo-init-android-o='repo init -u https://aosp.tuna.tsinghua.edu.cn/platform/manifest -b android-8.0.0_r13'
+alias repo-init-android-p='repo init -u https://aosp.tuna.tsinghua.edu.cn/platform/manifest -b android-9.0.0_r3'
