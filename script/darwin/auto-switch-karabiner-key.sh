dcc31c6994eac8bab5108001f30bad005f479212
Yang Liu <ayliu@amazon.com>
2018-09-24 22:22:44 +0800

    Move the auto-switch-karabiner-key.sh to common folder.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

---
+++ script/darwin/auto-switch-karabiner-key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+external_keyboard_exist=`system_profiler SPUSBDataType | grep "Rapoo Gaming Keyboard"`
+external_keyboard_exist2=`system_profiler SPUSBDataType | grep "CHERRY"`
+
+if [ -z "$external_keyboard_exist" ] && [ -z "$external_keyboard_exist2"]; then
+    echo "switch to use configuration for internal keyboard"
+    cp $HOME/vif/karabiner-internal.json ~/.karabiner.d/configuration/karabiner.json
+else
+    echo "switch to use configuration for external keyboard"
+    cp $HOME/vif/karabiner-external.json ~/.karabiner.d/configuration/karabiner.json
+fi
