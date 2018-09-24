1f860e665ea2d17648d0f4561b94a17543a7c867
chophi <chophi@qq.com>
2018-09-24 22:52:30 +0800

    Fix syntax error.
    Signed-off-by: chophi <chophi@qq.com>

--- script/darwin/auto-switch-karabiner-key.sh
+++ script/darwin/auto-switch-karabiner-key.sh
@@ -3,7 +3,7 @@
 external_keyboard_exist=`system_profiler SPUSBDataType | grep "Rapoo Gaming Keyboard"`
 external_keyboard_exist2=`system_profiler SPUSBDataType | grep "CHERRY"`
 
-if [ -z "$external_keyboard_exist" ] && [ -z "$external_keyboard_exist2"]; then
+if [ -z "$external_keyboard_exist" ] && [ -z "$external_keyboard_exist2" ]; then
     echo "switch to use configuration for internal keyboard"
     cp $HOME/vif/karabiner-internal.json ~/.karabiner.d/configuration/karabiner.json
 else
