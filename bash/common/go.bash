5b990a986badb0bd49bc0bc704d612c6e9e6033b
chophi@A_MAC <chophi@foxmail.com>
2019-04-10 13:24:59 +0800

    Add some go util functions.
    Signed-off-by: chophi@A_MAC <chophi@foxmail.com>

--- bash/common/go.bash
+++ bash/common/go.bash
@@ -1,19 +1,36 @@
-function set-go-env-prefer-go1.9 {
-    if [ -e "/usr/local/Cellar/go@1.9/bin" ]; then
-        export PATH=/usr/local/Cellar/go@1.9/bin:$PATH
+function _set-go-env-unset-go1.9 {
+    local x=${PATH/\/usr\/local\/Cellar\/go@1.9\/1.9.7\/bin:/}
+    export PATH=$x
+}
+
+function _set-go-env-prefer-go1.9 {
+    if [ -e /usr/local/Cellar/go@1.9/1.9.7/bin ]; then
+        export PATH=/usr/local/Cellar/go@1.9/1.9.7/bin:$PATH
     fi
 }
 
-function set-go-env-mit-6.824 {
+function _set-go-env-mit-6.824 {
+    _set-go-env-prefer-go1.9
     if [ -e $HOME/git-repo/mit-6.824 ]; then
         export GOPATH=$HOME/git-repo/mit-6.824
         emacsclient -s $EMACS_SOCKET_NAME -e "(cu-set-gopath \"$GOPATH\")"
     fi
 }
 
-function set-go-env-go-tour {
+function _set-go-env-go-tour {
     if [ -e $HOME/git-repo/go_tour ]; then
         export GOPATH=$HOME/git-repo/go_tour
         emacsclient -s $EMACS_SOCKET_NAME -e "(cu-set-gopath \"$GOPATH\")"
     fi
+}
+
+function set-go-env {
+    echo "Make your choice: "
+    local menu=(
+        "unset-go1.9: _set-go-env-unset-go1.9"
+        "use-go1.9: _set-go-env-prefer-go1.9"
+        "mit-6.824: _set-go-env-mit-6.824"
+        "go_tour: _set-go-env-go-tour"
+    )
+    run_item_in_menu "${menu[@]}"
 }
