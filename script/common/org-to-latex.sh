25d40e190a4fcff8ba8bcd4ddcea864fe59913f6
chophi@A_MAC <chophi@foxmail.com>
2019-04-23 12:13:24 +0800

    Update the script.
    Signed-off-by: chophi@A_MAC <chophi@foxmail.com>

--- script/common/org-to-latex.sh
+++ script/common/org-to-latex.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-   echo org-to-latex <name> [cn]
+    echo "Usage: org-to-latex <name> [cn]"
+    exit 0
 fi
 
-filename=$1
+filename=$(realpath $1)
+
+if ![ -e $filename ]; then
+    echo "$filenane is not exist!"
+    exit 1
+fi
+
+md5sum=$(echo $filename | md5sum | cut -d " " -f 1)
 bn=`basename $filename .org`
-emacsclient -s ~/.emacs.d/server/server -e "(with-current-buffer (get-file-buffer \"$filename\") (org-latex-export-to-latex))"
-mkdir -p tmp
-rm -rf tmp/*
-mv ${bn}.tex tmp/${bn}.tex
+tmp_dir=/tmp/${bn}-${md5sum}
+mkdir -p ${tmp_dir}
+rm -rf ${tmp_dir}/*
+cp -f $filename $tmp_dir
+
+emacsclient \
+    -s ~/.emacs.d/server/server \
+    -e \
+    "(cu-offline-org-to-latex \"$tmp_dir/$(basename $filename)\" \"$tmp_dir/${bn}.tex\")"
 
 if [ $# -ge 2 ] && [ "$2" == cn ]; then
-    (cd tmp && latexmk -pdf -f -xelatex -shell-escape ${bn}.tex)
+    (cd ${tmp_dir} && latexmk -pdf -f -xelatex -shell-escape ${bn}.tex)
 else
-    (cd tmp && latexmk -pdf -f -shell-escape ${bn}.tex)
+    (cd ${tmp_dir} && latexmk -pdf -f -shell-escape ${bn}.tex)
 fi
 
-mv tmp/${bn}.pdf .
+mkdir -p ${HOME}/ORG-PDF
+mv ${tmp_dir}/${bn}.pdf ${HOME}/ORG-PDF
