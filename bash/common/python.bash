9e6b3f3dad716e57154574245ffb1dfbdacdd6d6
chophi <chophi@qq.com>
2019-05-04 21:24:56 +0800

    Use pip mirror.
    Signed-off-by: chophi <chophi@qq.com>

--- bash/common/python.bash
+++ bash/common/python.bash
@@ -39,3 +39,15 @@ function choose-python-virtualenv {
         clean-variable PATH
     fi
 }
+
+# mirrors of pip
+# 阿里云 https://mirrors.aliyun.com/pypi/simple/
+# 中国科技大学 https://pypi.mirrors.ustc.edu.cn/simple/
+# 豆瓣(douban) http://pypi.douban.com/simple/
+# 清华大学 https://pypi.tuna.tsinghua.edu.cn/simple/
+# 中国科学技术大学 http://pypi.mirrors.ustc.edu.cn/simple/
+function pip-install {
+    sudo -H pip install "$@" -i https://mirrors.aliyun.com/pypi/simple/
+}
+
+
