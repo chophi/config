677b436a5a43fafc7c43c8ab76411115a50ef729
chophi <chophi@qq.com>
2019-05-04 11:23:47 +0800

    Use python3 by-default.
    Signed-off-by: chophi <chophi@qq.com>

--- bash/common/post.bash
+++ bash/common/post.bash
@@ -1,3 +1,13 @@
+if [ -e $HOME/.py-vir-env/py3 ]; then
+    export PYTHONHOME=$HOME/.py-vir-env/py3
+    export VIRTUAL_ENV=$HOME/.py-vir-env/py3
+    export PATH=$VIRTUAL_ENV/bin:$PATH
+elif [ -e $HOME/.py-vir-env/py2 ]; then
+    export PYTHONHOME=$HOME/.py-vir-env/py2
+    export VIRTUAL_ENV=$HOME/.py-vir-env/py2
+    export PATH=$VIRTUAL_ENV/bin:$PATH
+fi
+
 clean-variable PATH
 clean-variable LD_LIBRARY_PATH
 clean-variable INFOPATH
---
+++ bash/common/python.bash
@@ -0,0 +1,41 @@
+function create-python-virtualenv {
+    if [ $# -lt 1 ] || ([ "$1" != "2" ] && [ "$1" != "3" ]); then
+        echo "Usage: create-python-virtualenv 2|3"
+        return 1
+    fi
+
+    if [ -e $HOME/.py-vir-env/py$1 ]; then
+        echo "Virtual env already exist in $HOME/.py-vir-env/py$1"
+        return 0
+    fi
+    mkdir -p ~/.py-vir-env
+    virtualenv --system-site-packages --python=python$1 $HOME/.py-vir-env/py$1
+}
+
+function choose-python-virtualenv {
+    deactive > /dev/null 2>&1
+    local choice
+    if [ -e $HOME/.py-vir-env/py3 ]; then
+        choice=3
+    fi
+    if [ -e $HOME/.py-vir-env/py2 ]; then
+        ((choice=choice+2))
+    fi
+
+    if [ $choice -gt 3 ]; then
+        echo -n "Both py2 and py3 are exist, input your choice(2/3): "
+        read choice
+        if [ "$choice" != "2" ] && [ "$choice" != "3" ]; then
+            echo "Bad choice, do nothing, exit!"
+            return 1
+        fi
+    fi
+
+    if [ -n "$choice" ]; then
+        export PYTHONHOME=$HOME/.py-vir-env/py$choice
+        export VIRTUAL_ENV=$HOME/.py-vir-env/py$choice
+        export PATH=$VIRTUAL_ENV/bin:$PATH
+        emacsclient -s $EMACS_SOCKET_NAME -e "(cu-set-python-virtualenv \"$PYTHONHOME\")"
+        clean-variable PATH
+    fi
+}
