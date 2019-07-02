fabfc715fa0f00fb17cb30a814b4598a0308d76f
chophi <chophi@foxmail.com>
2019-07-02 15:09:46 +0800

    Add some variables to locate the correct paths.
    Signed-off-by: chophi <chophi@foxmail.com>

--- script/darwin/auto-switch-karabiner-key.sh
+++ script/darwin/auto-switch-karabiner-key.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+. ~/.bash_environment_setup
 external_keyboard_exist=`system_profiler SPUSBDataType | grep "Rapoo Gaming Keyboard"`
 external_keyboard_exist2=`system_profiler SPUSBDataType | grep "CHERRY"`
 external_keyboard_exist3=`system_profiler SPUSBDataType | grep "Holtek Semiconductor"`
 
 if [ -z "$external_keyboard_exist" ] && [ -z "$external_keyboard_exist2" ] && [ -z "$external_keyboard_exist3" ]; then
     echo "switch to use configuration for internal keyboard"
-    cp $HOME/vif/karabiner-internal.json ~/.karabiner.d/configuration/karabiner.json
+    cp $VIF_CONFIG/karabiner-internal.json ~/.karabiner.d/configuration/karabiner.json
 else
     echo "switch to use configuration for external keyboard"
-    cp $HOME/vif/karabiner-external.json ~/.karabiner.d/configuration/karabiner.json
+    cp $VIF_CONFIG/karabiner-external.json ~/.karabiner.d/configuration/karabiner.json
 fi
