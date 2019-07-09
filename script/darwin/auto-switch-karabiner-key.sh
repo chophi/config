6f16e9df6f33c49fb0e37fb93d50187d80d1a1ec
chophi <chophi@foxmail.com>
2019-07-09 19:39:26 +0800

    The karabiner folder might be different.
    Signed-off-by: chophi <chophi@foxmail.com>

--- script/darwin/auto-switch-karabiner-key.sh
+++ script/darwin/auto-switch-karabiner-key.sh
@@ -4,10 +4,14 @@ external_keyboard_exist=`system_profiler SPUSBDataType | grep "Rapoo Gaming Keyb
 external_keyboard_exist2=`system_profiler SPUSBDataType | grep "CHERRY"`
 external_keyboard_exist3=`system_profiler SPUSBDataType | grep "Holtek Semiconductor"`
 
+karabiner_config_folder=~/.karabiner.d/configuration
+if [ -e ~/.config/karabiner ] && ! [ -e $karabiner_config_folder ] ; then
+    karabiner_config_folder=~/.config/karabiner
+fi
 if [ -z "$external_keyboard_exist" ] && [ -z "$external_keyboard_exist2" ] && [ -z "$external_keyboard_exist3" ]; then
     echo "switch to use configuration for internal keyboard"
-    cp $VIF_CONFIG/karabiner-internal.json ~/.karabiner.d/configuration/karabiner.json
+    cp $VIF_CONFIG/karabiner-internal.json $karabiner_config_folder/karabiner.json
 else
     echo "switch to use configuration for external keyboard"
-    cp $VIF_CONFIG/karabiner-external.json ~/.karabiner.d/configuration/karabiner.json
+    cp $VIF_CONFIG/karabiner-external.json $karabiner_config_folder/karabiner.json
 fi
